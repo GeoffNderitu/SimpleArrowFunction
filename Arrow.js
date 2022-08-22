f3275770d207a909e3b32264e588055b90c94c08
GeoffNderitu <geoffreynderitun@gmail.com>
2022-08-22 12:07:03 +0300

    Simple Arrow Function Example

---
+++ Arrow.js
@@ -0,0 +1,19 @@
+let onyesha = (a,b,c)=>{
+   if(a > b && a > c){
+    alert("The highest number is " + a);
+   }else if(a < b && a < c){
+   alert("The lowest number is " + a);
+} 
+if(b > a && b > c){
+    alert("The highest number is " + b);
+   }else if(b < a && b < c){
+   alert("The lowest number is " + b);
+}
+if(c > a && c > b){
+    alert("The highest number is " + c);
+   }else if(c < a && c < b){
+   alert("The lowest number is " + c);
+}
+}
+onyesha(45,60,78)
+
